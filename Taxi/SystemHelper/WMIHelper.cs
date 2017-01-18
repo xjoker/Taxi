170805bcad5707294391dd5bd205d4d98fcca23b
xJoker <xjoker@xjoker.us>
2017-01-18 10:05:25 +0800

    WMI 帮助类 增加远程链接功能

--- Taxi/SystemHelper/WMIHelper.cs
+++ Taxi/SystemHelper/WMIHelper.cs
@@ -10,13 +10,56 @@ namespace Taxi.SystemHelper
     public static class WMIHelper
     {
         /// <summary>
+        /// WMI 远程查询帮助类(String)
+        /// </summary>
+        /// <param name="ipAddress">远程IP</param>
+        /// <param name="remoteUsername">远程主机用户名</param>
+        /// <param name="remotePassword">远程主机密码</param>
+        /// <param name="selectQuery"></param>
+        /// <returns></returns>
+        public static List<Dictionary<string, string>> WMIRemoteQueryHelper(string ipAddress, string remoteUsername, string remotePassword, string selectQuery)
+        {
+            return WMIRemoteQueryHelper(ipAddress, remoteUsername, remotePassword, new SelectQuery(selectQuery));
+        }
+
+
+        /// <summary>
+        /// WMI 远程查询帮助类(SelectQuery)
+        /// </summary>
+        /// <param name="ipAddress">远程IP</param>
+        /// <param name="remoteUsername">远程主机用户名</param>
+        /// <param name="remotePassword">远程主机密码</param>
+        /// <param name="selectQuery"></param>
+        /// <returns></returns>
+        public static List<Dictionary<string, string>> WMIRemoteQueryHelper(string ipAddress, string remoteUsername, string remotePassword, SelectQuery selectQuery)
+        {
+            ConnectionOptions op = new ConnectionOptions();
+            op.Username = remoteUsername;
+            op.Password = remotePassword;
+            var scope = new ManagementScope($"\\\\{ipAddress}\\root\\cimv2", op);
+            return WMIRemoteQueryHelper(scope, selectQuery);
+        }
+
+        /// <summary>
+        /// WMI 远程查询帮助类
+        /// </summary>
+        /// <param name="ms"></param>
+        /// <param name="selectQuery"></param>
+        /// <returns></returns>
+        public static List<Dictionary<string, string>> WMIRemoteQueryHelper(ManagementScope ms, SelectQuery selectQuery)
+        {
+            return WMIQueryHelper(ms, selectQuery);
+        }
+
+
+        /// <summary>
         /// WMI 查询帮助类(String)
         /// </summary>
         /// <param name="selectQuery">String类型查询语句传入</param>
         /// <returns></returns>
         public static List<Dictionary<string, string>> WMIQueryHelper(string selectQuery)
         {
-            return WMIQueryHelper(new SelectQuery(selectQuery));
+            return WMIQueryHelper(new ManagementScope("\\root\\cimv2"), new SelectQuery(selectQuery));
         }
 
         /// <summary>
@@ -24,12 +67,12 @@ namespace Taxi.SystemHelper
         /// </summary>
         /// <param name="sq">SelectQuery类型查询传入</param>
         /// <returns></returns>
-        public static List<Dictionary<string, string>> WMIQueryHelper(SelectQuery sq)
+        public static List<Dictionary<string, string>> WMIQueryHelper(ManagementScope scope, SelectQuery sq)
         {
             try
             {
                 var l = new List<Dictionary<string, string>>();
-                var mos = new ManagementObjectSearcher(sq).Get();
+                var mos = new ManagementObjectSearcher(scope, sq).Get();
                 foreach (ManagementObject bus in mos)
                 {
                     var d = new Dictionary<string, string>();
