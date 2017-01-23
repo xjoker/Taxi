8eec9aa4adb3120e53af93f9461032bf3ca634d4
xJoker <xjoker@xjoker.us>
2017-01-23 13:08:42 +0800

    增加网络帮助模块，增加 PING 和 Domain 解析

---
+++ Taxi/Network/NetworkTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using Taxi.SystemHelper;
+using Taxi.StringHelper;
+
+namespace Taxi.Network
+{
+    class NetworkTools
+    {
+        /// <summary>
+        /// Ping测试
+        /// </summary>
+        /// <param name="host">主机或IP</param>
+        /// <returns>返回延时，如果不通则返回NULL</returns>
+        public static string Ping(string host)
+        {
+            var p = WMIHelper.WMIQueryHelper(new ManagementScope("\\root\\cimv2"), new SelectQuery("Win32_PingStatus", $"Address='{host}'"));
+            return p[0]["ResponseTime"];
+        }
+
+        /// <summary>
+        /// 域名解析为IP地址
+        /// </summary>
+        /// <param name="host">主机或IP</param>
+        /// <returns>返回IP，如果未查询到则返回NULL</returns>
+        public static string DomainToIP(string host)
+        {
+            var p = WMIHelper.WMIQueryHelper(new ManagementScope("\\root\\cimv2"), new SelectQuery("Win32_PingStatus", $"Address='{host}'"));
+            return p[0]["ProtocolAddress"].IsNullOrWhiteSpace()?null: p[0]["ProtocolAddress"];
+        }
+    }
+}
