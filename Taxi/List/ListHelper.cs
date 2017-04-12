f7aa94e35c4ed8cf50b2174220afea1078923922
xJoker <xjoker@xjoker.us>
2017-04-12 11:23:03 +0800

    增加ini文件读取功能 List 搜索功能 获取最后一个元素的功能

---
+++ Taxi/List/ListHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi.List
+{
+    public static class ListHelper
+    {
+        /// <summary>
+        /// 获取List内最后一个元素
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static T GetLast<T>(this List<T> list)
+        {
+            return list[list.Count - 1];
+        }
+
+        /// <summary>
+        /// List 搜索功能
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static List<string> Search(this List<string> t,string word)
+        {
+            return t.Where(e => e.ToLower().Contains(word.ToLower())).ToList();
+        }
+    }
+}
