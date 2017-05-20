042b58dea4300d5e4552fced4c48f1d379d9831c
xJoker <xjoker@xjoker.us>
2017-05-20 20:52:06 +0800

    ### 随机帮助类 * RandomHelper.GenerateRandomNumber 生成随机数字,取值范围为(0-9) * RandomHelper.GenerateRandomString 生成随机字符串,字符范围为(0-9 a-b A-B) * RandomHelper.GenerateRandomString 生成随机字符串,字符范围为(a-b)或者(A-B) * RandomHelper.GenerateRandomPassword 生成随机密码 * RandomHelper.GetRandomString  随机类的基础生成方法

---
+++ TaxiTests/RandomHelper/RandomHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taxi.RandomHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi.RandomHelper.Tests
+{
+    [TestClass()]
+    public class RandomHelperTests
+    {
+        [TestMethod()]
+        public void GenerateRandomNumberTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GenerateRandomStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GenerateRandomStringTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GenerateRandomPasswordTest()
+        {
+            var b = RandomHelper.GenerateRandomPassword(10,includeNumber:false,includeMixedCase:false,includePunctuation:false);
+            Assert.IsNotNull(b);
+        }
+
+        [TestMethod()]
+        public void GetRandomStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
